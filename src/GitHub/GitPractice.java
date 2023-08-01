9a9c21bc316fbd3d010a29586f4c48f027bdd1f0
rakhimamirzayeva <rakhimamirzayeva91@gmail.com>
2023-08-01 17:13:35 -0400

    Second commit

--- src/GitHub/GitPractice.java
+++ src/GitHub/GitPractice.java
@@ -3,5 +3,6 @@ package GitHub;
 public class GitPractice {
     public static void main(String[] args) {
         System.out.println("Learning Git");
+        System.out.println("Making a change");
     }
 }
