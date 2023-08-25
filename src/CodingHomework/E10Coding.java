e8f71e08946c09e52787060ebda9fc77a19de18c
rakhimamirzayeva <rakhimamirzayeva91@gmail.com>
2023-08-24 22:18:11 -0400

    Coding

--- src/CodingHomework/E10Coding.java
+++ src/CodingHomework/E10Coding.java
@@ -3,7 +3,14 @@ package CodingHomework;
 public class E10Coding {
     public static void main(String[] args) {
        String[] country={"Turkey","Russia","Turkey","USA","China","Italy"};
-
+        for (int i = 0; i <country.length ; i++) {
+            for (int j =i+1; j <country.length ; j++) {
+                if(country[i]==country[j]){
+                    System.out.println("The duplicate country of this array is "+country[i]);
+                    break;
+                }
+            }
+        }
 
     }
 }
