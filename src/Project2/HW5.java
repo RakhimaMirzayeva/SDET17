f68667d62126eed724a716b0d186c46b2a748493
rakhimamirzayeva <rakhimamirzayeva91@gmail.com>
2023-09-22 21:45:19 -0400

    Project2

--- src/Project2/HW5.java
+++ src/Project2/HW5.java
@@ -8,23 +8,42 @@ public class HW5 {
 they are anagrams, meaning they contain the same characters in a
 different order. For example, "listen" and "silent" are anagrams.
     */
-    public static void main(String[] args) {
-        String word1="listen";
-        String word2="silent";
-        if(word1.length() !=word2.length()){
-            System.out.println("This is not an anagram");
-            char[] charArray1=word1.toCharArray();
-            char[] charArray2=word2.toCharArray();
+
+    public static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+        if (str1.length() != str2.length()) {
+
+            return false;
+        }
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
             Arrays.sort(charArray1);
             Arrays.sort(charArray2);
-            if(Arrays.equals(charArray1,charArray2)){
+            if (Arrays.equals(charArray1, charArray2)) {
                 System.out.println("This words are anagrams");
-            }else{
+            } else {
                 System.out.println("This words are not anagrams");
             }
-
+            return Arrays.equals(charArray1, charArray2);
         }
+
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("\"" + str1 + "\" and \"" + str2 + "\" are anagrams.");
+        } else {
+            System.out.println("\"" + str1 + "\" and \"" + str2 + "\" are not anagrams.");
+        }
+        }
+
     }
-}
+
+
+
+
 
 
